227d4ae0590c0ad4698aeea5e999e42f5cc2ac72
andrey <andrash00@gmail.com>
2020-04-09 18:57:20 +0300

    Fix

--- library/src/main/java/ru/otus/library/Main.java
+++ library/src/main/java/ru/otus/library/Main.java
@@ -2,36 +2,10 @@ package ru.otus.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.otus.library.dao.BookDao;
-import ru.otus.library.model.Author;
-import ru.otus.library.model.Genre;
-import ru.otus.library.service.LibraryService;
-
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        ConfigurableApplicationContext cont = SpringApplication.run(Main.class);
-
-        BookDao bookDao = cont.getBean(BookDao.class);
-
-        System.out.println(bookDao.findBooksByAuthor("автор2"));
-
-//        LibraryService libraryService = cont.getBean(LibraryService.class);
-//
-//        Author author = new Author();
-//        author.setName("Автор 1");
-//        author.setCountry("Россия");
-//        libraryService.addAuthor(author);
-//
-//        libraryService.findAuthorByName("Автор 1").ifPresent(System.out::println);
-//
-//        Genre genre = new Genre();
-//        genre.setName("Жанр 1");
-//        libraryService.addGenre(genre);
-//
-//        libraryService.findGenreByName("Жанр 1").ifPresent(System.out::println);
-
+       SpringApplication.run(Main.class);
     }
 }
